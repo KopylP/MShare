8736d0b08544330391f39ac64412693334004acf
KopylP <p.kopyl9@gmail.com>
2022-08-31 20:03:07 +0200

    Code correction

---
+++ src/MShare.Framework.Types/IntRange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MShare.Framework.Types
+{
+	public record IntRange
+	{
+		public int From { get; private init; }
+		public int To { get; private init; }
+
+		private IntRange(int from, int to)
+		{
+			if (from > to)
+				throw new ArgumentException("'From' should be equal or less then 'to'");
+
+			From = from;
+			To = to;
+		}
+
+		public static IntRange Of(int from, int to) => new IntRange(from, to);
+		public static IntRange Of(int to) => new IntRange(0, to);
+    }
+}
+
