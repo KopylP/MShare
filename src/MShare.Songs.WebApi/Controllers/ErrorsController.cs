ba7795df2b0adc38eb1eca9d47672c50f1e91c66
KopylP <p.kopyl9@gmail.com>
2022-09-18 20:47:21 +0200

    Add generic error model

---
+++ src/MShare.Songs.WebApi/Controllers/ErrorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MShare.Framework.WebApi;
+using System.Net;
+
+namespace MShare.Songs.WebApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/errors")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : Controller
+    {
+        [HttpGet("{code}")]
+        public IActionResult Error(int code)
+        {
+            var parsedCode = (HttpStatusCode)code;
+            var error = new ApiError(code, parsedCode.ToString());
+
+            return new ObjectResult(error);
+        }
+    }
+}
+
