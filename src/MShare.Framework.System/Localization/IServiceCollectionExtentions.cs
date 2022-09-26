940beb6dc0b35f6ee4fe5f427128ca191321bce0
KopylP <p.kopyl9@gmail.com>
2022-09-27 00:29:07 +0200

    Add localization support

---
+++ src/MShare.Framework.System/Localization/IServiceCollectionExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class IServiceCollectionExtentions
+	{
+		public static IServiceCollection AddSystemLocalization(this IServiceCollection services)
+        {
+			services.AddLocalization();
+			return services;
+        }
+    }
+}
+
---
+++ src/MShare.Framework.System/Localization/WebApplicationExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+
+namespace MShare.Framework.System.Localization
+{
+	public static class WebApplicationExtentions
+	{
+		public static IApplicationBuilder UseSystemLocalization(this IApplicationBuilder builder)
+		{
+			builder.UseRequestLocalization(opt =>
+			{
+				var supportedCultures = new string[] { "uk", "pl", "en" };
+				opt.AddSupportedCultures(supportedCultures);
+				opt.AddSupportedUICultures(supportedCultures);
+                opt.DefaultRequestCulture = new RequestCulture("en");
+				opt.RequestCultureProviders.Clear();
+				opt.RequestCultureProviders.Add(new AcceptLanguageHeaderRequestCultureProvider());
+			});
+
+			return builder;
+        }
+	}
+}
+
