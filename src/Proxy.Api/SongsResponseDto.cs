fa3681183461bb102fc52fde5fef1f8bfa59728e
KopylP <p.kopyl9@gmail.com>
2022-09-27 19:19:15 +0200

    code refactor

--- src/Proxy.Api/SongsResponseDto.cs
+++ src/Proxy.Api/SongsResponseDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using Unidecode.NET;
 
 namespace Proxy.Api
 {
@@ -8,6 +9,46 @@ namespace Proxy.Api
 
         [JsonIgnore]
         public bool IsEmpty => !Items?.Any() ?? true;
+    }
+
+    public static class SongResponseDtoExtentions
+    {
+        public static SongsResponseDto FilterItemsByArtistName(this SongsResponseDto response, string artistName)
+        {
+            if (!string.IsNullOrWhiteSpace(artistName) && response.Items is not null)
+            {
+                var lengthRange = (
+                    Min: artistName.Unidecode().Length - 1,
+                    Max: artistName.Unidecode().Length + 1);
+
+                response.Items = response.Items.Where(item =>
+                {
+                    var artists = item.Artists.First().Name.Split("&").Select(p => p.Unidecode());
+                    return artists.Any(artist => artist.Length >= lengthRange.Min && artist.Length <= lengthRange.Max);
+                })
+                .ToArray();
+            }
+
+            return response;
+        }
+
+        public static SongsResponseDto FilterItemsBySongName(this SongsResponseDto response, string songName)
+        {
+            if (!string.IsNullOrWhiteSpace(songName) && response is not null)
+            {
+                var items = response
+                    .Items
+                    .Where(item => item.Song.Name.ToLower() == songName.ToLower())
+                    .ToArray();
+
+                if (items.Any())
+                {
+                    response.Items = items;
+                }
+            }
+
+            return response;
+        }
     }
 }
 
