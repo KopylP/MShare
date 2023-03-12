40270c5dc23b6523b48fbc810cc3f23a17de37da
KopylP <p.kopyl9@gmail.com>
2023-03-12 23:14:39 +0100

    Add DbMigrator

---
+++ src/MShare.DbMigrator/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+IConfiguration Configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables("MSHARE_DB_")
+    .Build();
+
+
+var upgrader =
+    DeployChanges.To
+        .PostgresqlDatabase(Configuration.GetConnectionString("DefaultConnection"))
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
